a287bffc01f474b431185300900b8c7a61243b05
Cecilia Garcia L M <ceci.garcialm@gmail.com>
2024-05-18 22:12:47 -0700

    Added second participant to coordinator

--- coordinator.cpp
+++ coordinator.cpp
@@ -1,24 +1,33 @@
 #include "2PC_Coordinator.h"
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    const string logFile = "log.txt";
-
-    if (argc < 4) {
-        cerr << "usage: coordinator hostname port message" << endl;
-        return EXIT_FAILURE;
-    }
-    int port = atoi(argv[2]);
+int process_port(char *argv) {
+    // Process args
+    int port = atoi(argv);
     if (port < 1 || port >= 1 << 16) {
         cerr << "invalid port " << port << endl;
         return EXIT_FAILURE;
     }
+    return port;
+}
 
-    Coordinator coordinator(argv[1], (u_short) port, logFile);
-    
-    // Request transaction
+int main(int argc, char *argv[]) {
+    const string logFile = "log.txt";
     Coordinator::Transaction t {1000, 10.01};
-    coordinator.perform_transaction(t);
+
+    if (argc < 4) {
+        cerr << "usage: coordinator hostname1 port1 hostname2 port2" << endl;
+        return EXIT_FAILURE;
+    }
+
+    int portOne = process_port(argv[2]);
+    int portTwo = process_port(argv[4]);
+
+    Coordinator coordinatorOne(argv[1], (u_short) portOne, logFile);
+    coordinatorOne.perform_transaction(t);
+
+    Coordinator coordinatorTwo(argv[1], (u_short) portTwo, logFile);
+    coordinatorTwo.perform_transaction(t);
     
     return EXIT_SUCCESS;
 }
